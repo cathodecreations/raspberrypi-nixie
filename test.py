551ebf676847c02491914c0649868e8a933067f1
Theodore Kotz <ted@kotz.us>
2017-12-09 21:12:46 -0500

    Reverted to point before adding all the clock specific changes

--- test.py
+++ test.py
@@ -1,48 +1,56 @@
 #!/usr/bin/python
 
 #import modules
-import socket
-from time import sleep
+import sys
+import time
 import RPi.GPIO as GPIO
-from subprocess import check_output, call, CalledProcessError
-from datetime import datetime, date
-from pytz import timezone
-
-try:
-	from mpd import MPDClient, ConnectionError
-	mpdClient = MPDClient()
-except ImportError:
-	print 'python-mpd not installed disabling mpdsupport'
-	mpdClient = None
+import xml.etree.ElementTree as ET
+import os
+from os import system
 
 #Number of active Nixie Tubes
 tubes = 6
-TZ = timezone('US/Eastern')
-MPD_HOST = "localhost"
-MPD_PORT = "6600"
-FRAME_TIME = 0.1
 
+#Decimal to Binary function
+def d2b(y):
+#	return bin(int(y))[2:]
+        return d2b(y/2) + [y%2] if y > 1 else [y]
 
 #Send a pulse out the indicated strobe pin
-def pulseGPIO(pin, direction=True, duration=0.001):
-	GPIO.output(pin, direction)
-	sleep(duration)
-	GPIO.output(pin, not(direction))
+def pulseGPIO(pin):
+	GPIO.output(pin, True)
+	time.sleep(0.001)
+	GPIO.output(pin, False)
 
 
 #Display digits on Nixies, bit by bit
 def nixiebit(digit):
 	digit = int(max(0, min( int(digit), 15)))
-	for d in reversed(range (0, 4)):
+	for d in range (3, -1, -1):
 		GPIO.output(11, bool(digit & (1 << d)) )
 		pulseGPIO(12)
+#	digitbin = d2b(x)
+#	#print digitbin
+#	arrsize = len(digitbin)	
+#	for d in range (0, 4-arrsize):
+#		GPIO.output(11, False)
+#		pulseGPIO(12)
+#		#print 'Wrote: 0 {EOD}'
+#	for d in range (0, min(arrsize,4)):
+##		GPIO.output(11, digitbin[d] == '1')
+#		GPIO.output(11, bool(digitbin[d]))
+#		pulseGPIO(12)
+#		#print 'Wrote: ',digitbin[d]
 
 
 #Display String of digits
-def nixieString(digitString):
-	for c in reversed(str(digitString)):
+def nixieString(x):
+	digitString = str(x)
+	lastIndex = len(digitString) - 1
+	#print "Number length: ",i
+	for i in range(lastIndex, -1, -1):
 		try:
-			nixiebit(int(c))
+			nixiebit(int(digitString[i]))
 		except ValueError:
 			nixiebit(15)
 	#Display number on Nixies
@@ -52,7 +60,7 @@ def nixieString(digitString):
 
 #Ask User for string to display
 def userNixieString():
-	userInput = str(raw_input('Number to Display ( or "exit" ) : '))
+	userInput = str(raw_input('Number to Display: '))
 	if userInput == 'exit':
 		return False
 	print tubes
@@ -68,103 +76,14 @@ def sweepNixieString():
 			x=emptyString[:i] + digit + emptyString[i:]
 			#print x
 			nixieString(x)
-			sleep(0.1)
+			time.sleep(0.1)
 		for i in range (tubes-2, 0, -1):
 			x=emptyString[:i] + digit + emptyString[i:]
 			#print x
 			nixieString(x)
-			sleep(0.1)
+			time.sleep(0.1)
 	return True
 
-
-def mpcString(client):
-	if(client):
-		if (mpcString.mpcHoldoff > 0):
-			mpcString.mpcHoldoff -= 1
-			if (mpcString.mpcHoldoff <= 0):
-				try:
-					client.connect(MPD_HOST, MPD_PORT)
-				except socket.error:
-					print "Failed to reconnect MPD client"
-			return dateTimeString()
-		try:
-			status=client.status()
-			if(mpcString.oldVolume!=status['volume']):
-				mpcString.volumeDisplayTimer = 10
-				mpcString.oldVolume = status['volume']
-			if(mpcString.volumeDisplayTimer > 0):
-				mpcString.volumeDisplayTimer -= 1
-				nixieString("  "+status['volume'].rjust(2,"0")+"  ")
-				sleep(FRAME_TIME)
-				return True			
-			#print status
-			if(status['state']=="play"):
-				#volume = status['volume']
-				songid = status['songid']
-				timeFields = status['time'].split(":")
-				#elTime = int(timeFields[0])
-				elMin, elSec = divmod(int(timeFields[0]), 60)
-				#totTime = time(second=int(timeFields[1]))
-				digitString = songid.rjust(2,"0") + str(elMin).rjust(2," ") + str(elSec).rjust(2,"0")
-				#print digitString
-				nixieString(digitString)
-				sleep(FRAME_TIME)
-				return True
-		except (socket.error, ConnectionError):
-			print "ConnectionError"
-			mpcString.mpcHoldoff = 300
-			try:
-				client.disconnect()
-#				client.timeout = FRAME_TIME
-#				client.connect(MPD_HOST, MPD_PORT)
-			except (socket.error, ConnectionError):
-				print "Cleanup-ConnectionError"
-	return dateTimeString()
-mpcString.oldVolume = 0
-mpcString.volumeDisplayTimer = 0
-mpcString.mpcHoldoff = 1
-
-		
-
-def dateTimeString():
-#	nixieString( datetime.now().strftime(" %H%M ") )
-	TIME_DATE_MAX_OFFSET = len(" HHMMSS CCYY MM DD      ") - 6
-	timeStamp = datetime.now(TZ)
-	if dateTimeString.offset > 0 and dateTimeString.offset <= TIME_DATE_MAX_OFFSET:
-		x = timeStamp.strftime(" %H%M%S %Y %m %d      ")
-		if dateTimeString.frame > 15:
-			dateTimeString.offset += dateTimeString.direction
-			dateTimeString.frame = 0
-	elif timeStamp.second == 13:
-		x = timeStamp.strftime(" %H%M%S %Y %m %d      ")
-		dateTimeString.offset=1
-		dateTimeString.frame = 0
-	elif timeStamp.microsecond < 500000:
-#		x = timeStamp.strftime("%Y%m%d %H%M%S%f") 
-		x = timeStamp.strftime("%H%M%S") 
-		dateTimeString.offset=0
-	else:
-		x = timeStamp.strftime("%H%M  ") 
-		dateTimeString.offset=0
-#		x = timeStamp.strftime("%Y%m%d %H%M%S%f") 
-#	if dateTimeString.offset > (len(x) - 9):
-#		dateTimeString.offset=(len(x) - 9)
-#		dateTimeString.direction=-1
-#		dateTimeString.frame = -12
-#	elif dateTimeString.offset < 0:
-#		dateTimeString.offset=0
-#		dateTimeString.direction=1
-#		dateTimeString.frame = -6
-#	#dateTimeString.offset=(len(x) - 9)
-#	dateTimeString.offset=0
-	nixieString( x[dateTimeString.offset:dateTimeString.offset+6] )
-	sleep(FRAME_TIME)
-	dateTimeString.frame += 10
-	return True;
-
-dateTimeString.offset = 0
-dateTimeString.direction = 1
-dateTimeString.frame = 0
 
 #init the GPIO pins
 GPIO.setwarnings(False)
@@ -179,32 +98,24 @@ GPIO.output(12, False)
 
 
 #I just have a thing for clean screens...
-#call("clear")
+system("clear")
 
 emptyString='     '
-keepLooping=True
+keepLooping=True;
 print 'Hit Ctrl-C to Exit'
 try:
-	if(mpdClient):
-		mpdClient.timeout = FRAME_TIME
-		#mpdClient.connect(MPD_HOST, MPD_PORT)
 	while keepLooping:
 		#keepLooping=sweepNixieString()
-		#keepLooping=dateTimeString()
-		keepLooping=mpcString(mpdClient)
-		#keepLooping=userNixieString()
-except KeyboardInterrupt:
+		keepLooping=userNixieString()
+except:
 	# Do normal cleanup
 	print "Exception detected"
 
 #Cleanup...
 nixieString('aaaaaa')
 print "Exiting..."
-if(mpdClient):
-	mpdClient.close()
-	mpdClient.disconnect()
 GPIO.cleanup()
-#call("clear")
+#system("clear")
 
 
 	
